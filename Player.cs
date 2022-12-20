e94c122a1745ea35ea58b61536f4898edc96583a
Justin <justin@kauz.ch>
2022-12-20 10:32:47 +0100

    feat: Player class

---
+++ Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsConsoleGame
+{
+    internal class Player : Character {
+        // class
+
+        // member
+
+        // const
+
+
+        // meth
+
+    }
+}
