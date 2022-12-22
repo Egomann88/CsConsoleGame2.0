7f5669af795bf94721c0c30b543b65a4b226a1f0
Justin <justin@kauz.ch>
2022-12-22 12:31:48 +0100

    feat: EnemyArena class

---
+++ EnemyArena.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsConsoleGame
+{
+    internal class EnemyArena : Enemy
+    {
+        // class
+
+        // mem
+
+        // const
+        public EnemyArena(byte pLvl, byte eId, bool isHard = false) :base(pLvl, eId, isHard) {
+            SetEnemyStats(pLvl, eId, isHard);
+        }
+
+        // meth
+
+        /// <summary>
+        /// chooses enemy and increases its starts with player level and hardmode
+        /// </summary>
+        /// <param name="pLvl">player level</param>
+        /// <param name="eId">enemy id</param>
+        /// <param name="isHard">hardmode enemy true/false</param>
+        protected override void SetEnemyStats(byte pLvl, byte eId, bool isHard) {
+            float multiplier = 1.0F;
+
+            // increase difficulty by increasing stat multiplier
+            if (isHard) multiplier += 0.75f;
+            while (pLvl - 10 >= 0) {
+                multiplier += 0.2F;
+                pLvl -= 10;
+            }
+
+            switch (eId) {
+                case 1:
+                    Name = "Gladiator";
+                    Strength = Convert.ToUInt16(Math.Round(12 * multiplier));
+                    Intelligents = Convert.ToUInt16(Math.Round(10 * multiplier));
+                    Dexterity = Convert.ToUInt16(Math.Round(10 * multiplier));
+                    CritChance = 0.10F * multiplier;
+                    CritMult = base.MaxMultiplier(1.1F, multiplier, true);
+                    Health = new short[] {
+                        Convert.ToInt16(Math.Round(60 * multiplier)),
+                        Convert.ToInt16(Math.Round(60 * multiplier))
+                    };
+                    Gold = Convert.ToInt32(Math.Round(334 * multiplier));
+                    Exp[0] = Convert.ToUInt16(Math.Round(227 * multiplier));
+                    IsDmgUlt = true;
+                    break;
+                case 2:
+                    Name = "Aurelion";
+                    Strength = Convert.ToUInt16(Math.Round(8.4 * multiplier));
+                    Intelligents = Convert.ToUInt16(Math.Round(14.1 * multiplier));
+                    Dexterity = Convert.ToUInt16(Math.Round(8.2 * multiplier));
+                    CritChance = 0.09F * multiplier;
+                    CritMult = base.MaxMultiplier(1.15F, multiplier, true);
+                    Health = new short[] {
+                        Convert.ToInt16(Math.Round(56.4 * multiplier)),
+                        Convert.ToInt16(Math.Round(56.4 * multiplier))
+                    };
+                    Gold = Convert.ToInt32(Math.Round(452 * multiplier));
+                    Exp[0] = Convert.ToUInt16(Math.Round(172 * multiplier));
+                    IsDmgUlt = false;
+                    break;
+                case 3:
+                    Name = "Gold Hexe";
+                    Strength = Convert.ToUInt16(Math.Round(5 * multiplier));
+                    Intelligents = Convert.ToUInt16(Math.Round(17.8 * multiplier));
+                    Dexterity = Convert.ToUInt16(Math.Round(5 * multiplier));
+                    CritChance = 0;
+                    CritMult = 0;
+                    Health = new short[] {
+                        Convert.ToInt16(Math.Round(30 * multiplier)),
+                        Convert.ToInt16(Math.Round(90 * multiplier))
+                    };
+                    Gold = Convert.ToInt32(Math.Round(384 * multiplier));
+                    Exp[0] = Convert.ToUInt16(Math.Round(207 * multiplier));
+                    IsDmgUlt = false;
+                    break;
+                case 4:
+                    Name = "Todesritter";
+                    Strength = Convert.ToUInt16(Math.Round(14.4 * multiplier));
+                    Intelligents = Convert.ToUInt16(Math.Round(10 * multiplier));
+                    Dexterity = Convert.ToUInt16(Math.Round(10 * multiplier));
+                    CritChance = 0.03F * multiplier;
+                    CritMult = base.MaxMultiplier(1.66F, multiplier, true);
+                    Health = new short[] {
+                        Convert.ToInt16(Math.Round(66 * multiplier)),
+                        Convert.ToInt16(Math.Round(75.4 * multiplier))
+                    };
+                    Gold = Convert.ToInt32(Math.Round(234 * multiplier));
+                    Exp[0] = Convert.ToUInt16(Math.Round(427 * multiplier));
+                    IsDmgUlt = true;
+                    break;
+                case 5:
+                    Name = "Nachtklinge";
+                    Strength = Convert.ToUInt16(Math.Round(11.4 * multiplier));
+                    Intelligents = Convert.ToUInt16(Math.Round(8.4 * multiplier));
+                    Dexterity = Convert.ToUInt16(Math.Round(16.4 * multiplier));
+                    CritChance = 0.20F * multiplier;
+                    CritMult = base.MaxMultiplier(1.11F, multiplier, true);
+                    Health = new short[] {
+                        Convert.ToInt16(Math.Round(40 * multiplier)),
+                        Convert.ToInt16(Math.Round(40 * multiplier))
+                    };
+                    Gold = Convert.ToInt32(Math.Round(350 * multiplier));
+                    Exp[0] = Convert.ToUInt16(Math.Round(350 * multiplier));
+                    IsDmgUlt = true;
+                    break;
+            }
+        }
+    }
+}
